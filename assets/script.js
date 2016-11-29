e174ec9c95089f66ae4ac2b68bddb7e6d62b8b73
hkarapetyan <hkarapetyan@berkeley.edu>
2016-11-28 21:49:15 -0800

    Hover - picture becomes colorful

--- assets/script.js
+++ assets/script.js
@@ -9,11 +9,46 @@ $(document).ready(function(){
 		}
 	});
 
-// Hover on circles
-	$('.slide-thumbnaillife').hover(
-    function() { $('.overlay').fadeIn(); },
-    function() { $('.overlay').fadeOut(); }
-  )
+	$('.slide-thumbnaillife').on({
+		mouseover: function() {
+			$(this).find('.overlay').stop(true, true).fadeIn();
+		},
+		mouseout: function() {
+			$(this).find('.overlay').stop(true, true).fadeOut();
+		}
+	});
 
+	$('.slide-thumbnailexp').on({
+		mouseover: function() {
+			$(this).find('.overlay').stop(true, true).fadeIn();
+		},
+		mouseout: function() {
+			$(this).find('.overlay').stop(true, true).fadeOut();
+		}
+	});
+
+	$('.slide-thumbnailconnect').on({
+		mouseover: function() {
+			$(this).find('.overlaysocial').stop(true, true).fadeIn();
+		},
+		mouseout: function() {
+			$(this).find('.overlaysocial').stop(true, true).fadeOut();
+		}
+	});
+
+	// Hover on circles
+		// $('.slide-thumbnaillife').hover(
+	  //   function() { $('.overlay').fadeIn(); },
+	  //   function() { $('.overlay').fadeOut(); }
+	  // )
+		$('#cover-background.img-scaled-h').click(function() {
+			alert('hi');
+		});
+
+	// $('#cover-background .img-scaled-h').hover(
+	//
+	// 	function() { alert('hi'); $('#cover-background .img-hover').fadeIn(); },
+	// 	function() { $('#cover-background .img-hover').fadeOut(); }
+	// )
 
 });
